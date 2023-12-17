f3992c2c4b3affa3b1374af114e6b8178028cf2b
FloatinggOnion <jesseosems123@gmail.com>
2023-12-17 14:46:02 +0100

    Update webhook link

--- app/main.py
+++ app/main.py
@@ -233,7 +233,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://cusc-bot.herokuapp.com/" + TOKEN)
+    bot.set_webhook(url="https://cusc-telegram-bot-kjz4.onrender.com" + TOKEN)
     return "!", 200
 
 
