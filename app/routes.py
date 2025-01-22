5387e3e3735631b4d868cacc0a568adc65776793
Ezekiel <ezekielakinfenwa98@gmail.com>
2025-01-22 13:12:01 +0100

    *

--- app/routes.py
+++ app/routes.py
@@ -23,14 +23,18 @@ bot = telebot.TeleBot(os.getenv("BOT_API_KEY"))
 webhook_url = os.getenv("WEBHOOK_URL")
 
 
-
 @routes_blueprint.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
     logging.info(data)
 
-    text = data["message"]["text"]
-    chat_id = data["message"]["chat"]["id"]
+    if data.get("message"):
+        text = data["message"]["text"]
+        chat_id = data["message"]["chat"]["id"]
+
+    if data.get("edited_message"):
+        text = data["edited_message"]["text"]
+        chat_id = data["edited_message"]["chat"]["id"]
 
     setup_user(bot, chat_id, text)
 
@@ -39,17 +43,17 @@ def webhook():
 
 @routes_blueprint.route("/announce", methods=["POST"])
 def announce():
-
     college = request.form["colleges"].split(" ")[0]
     level = request.form["levels"].split(" ")[0]
     files = request.files
     data = request.form
 
     if not data["message"] and not files:
-        return render_template('messages/failure.html', reason = "No message or file was sent")
+        return render_template(
+            "messages/failure.html", reason="No message or file was sent"
+        )
 
     add_message(data["message"], college, level)
-    
 
     all_recipients = filter_users(college, level).data
     if files:
@@ -59,13 +63,11 @@ def announce():
         caption = file_name if not data["message"] else data["message"]
 
         if file_name.endswith(("jpg", "jpeg", "png")):
-
             for user in all_recipients:
                 bot.send_photo(user.chat_id, uploaded_file, caption)
-                return render_template('messages/success.html')
+                return render_template("messages/success.html")
 
         elif file_name.endswith(("docx", "pdf", "xlsx", "pptx")):
-
             for user in all_recipients:
                 bot.send_document(
                     user["chat_id"],
@@ -74,23 +76,24 @@ def announce():
                     visible_file_name=file_name,
                 )
 
-            return render_template('messages/success.html')
+            return render_template("messages/success.html")
 
     for user in all_recipients:
         bot.send_message(user["chat_id"], data["message"])
-    
 
-    return render_template('messages/success.html')
+    return render_template("messages/success.html")
 
 
 @routes_blueprint.route("/")
 def index():
     return render_template("index.html")
 
+
 @routes_blueprint.route("/messages", methods=["GET"])
 def messages():
     messages = get_messages()
     return render_template("messages.html", messages=messages)
+
 
 bot.remove_webhook()
 bot.set_webhook(webhook_url)
